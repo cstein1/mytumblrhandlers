ac4d182a54802c8c5fa782f273e6c0e3efedd985
cstein1 <cstein1@no-reply.github.com>
2023-02-07 23:39:53 -0600

    starting on GetPosts method; able to retrieve miniposts and other content, but cannot find the text!

--- src/clientactions.go
+++ src/clientactions.go
@@ -2,8 +2,12 @@ package mytumblrhandlers
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/url"
+	"time"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/tumblr/tumblr.go"
 )
 
 func (t *MyTumblrHandler) GetBlogObj(blogName string) (interface{}, error) {
@@ -32,4 +36,27 @@ func (t *MyTumblrHandler) GetBlogInfo(blogName string) (string, error) {
 	}
 	blogInfo, err := json.MarshalIndent(blog, "", "\t")
 	return string(blogInfo), err
+}
+
+func (t *MyTumblrHandler) GetPosts(blogObj *tumblr.BlogRef) (string, error) {
+	err := t.IsValid()
+	if err != nil {
+		return "", err
+	}
+	v := url.Values{}
+	// TODO: these need lots of work... Offset required
+	v.Add("before", time.Now().GoString())
+	v.Add("limit", "3")
+	log.Tracef("getting posts from blogref: %s", blogObj.Name)
+	posts, err := blogObj.GetPosts(v)
+	for _, post := range posts.Posts {
+		fmt.Printf("POST: %s\n", post.Id)
+		postObj := t.Client.GetPost(post.Id, blogObj.Name)
+		if postObj != nil {
+			fmt.Printf("post: %v\n", postObj)
+		} else {
+			fmt.Printf("No post\n")
+		}
+	}
+	return "", nil
 }
